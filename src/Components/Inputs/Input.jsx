a03a87ccbe45308d7645431b918e5f3dba2e297f
santiagor06 <santiago.restrepo06@hotmail.com>
2023-12-10 14:35:35 -0500

    select

--- src/Components/Inputs/Input.jsx
+++ src/Components/Inputs/Input.jsx
@@ -1,48 +1,61 @@
 import './Input.css'
-import React, { useState } from 'react';
+
 
 const Input = () => {
-  const [mostrarMenu, setMostrarMenu] = useState(false);
-  const [opcionSeleccionada, setOpcionSeleccionada] = useState(null);
+  // const [mostrarMenu, setMostrarMenu] = useState(false);
+  // const [opcionSeleccionada, setOpcionSeleccionada] = useState(null);
 
   const opciones = [
-    'Horas cargables al cliente',
-    'Horas no cargables al cliente',
-    'Horas por requerimiento cargables al cliente',
-    'Horas por requerimiento cargables al cliente',
-    'General Sophos cargable',
-    'General Sophos NO cargable',
+    {
+      name:'Horas cargables al cliente',
+      value:"GetTimeEntries"
+    }
+    ,
+    {
+      name:'Horas cargables al cliente',
+      value:"GetTimeEntries"
+    }
+    ,
+    {
+      name:'Horas cargables al cliente',
+      value:"GetTimeEntries"
+    }
+    
+    // 'Horas no cargables al cliente',
+    // 'Horas por requerimiento cargables al cliente',
+    // 'Horas por requerimiento cargables al cliente',
+    // 'General Sophos cargable',
+    // 'General Sophos NO cargable',
   ];
 
-  const handleToggleMenu = () => {
-    setMostrarMenu(!mostrarMenu);
-  };
+  // const handleToggleMenu = () => {
+  //   setMostrarMenu(!mostrarMenu);
+  // };
 
-  const handleOpcionSeleccionada = (opcion) => {
-    setOpcionSeleccionada(opcion);
-    setMostrarMenu(false);
-  };
+  // const handleOpcionSeleccionada = (opcion) => {
+  //   setOpcionSeleccionada(opcion);
+  //   setMostrarMenu(false);
+  // };
 
   return (
     <div className="contenedor">
       <div className="desplegable">
-        <button className="boton-principal" onClick={handleToggleMenu}>
-          {opcionSeleccionada ? opcionSeleccionada : 'Selecciona una opción'}
-        </button>
-        {mostrarMenu && (
-          <div className="contenido-desplegable">
-            {opciones.map((opcion, index) => (
-              <button
-                key={index}
-                className="boton-opcion"
-                onClick={() => handleOpcionSeleccionada(opcion)}
-              >
-                {opcion}
-              </button>
-            ))}
+      <select>
+        {
+          opciones.map((opciones,index)=>{
+            return(
+              <option value={opciones.value} key={index}>
+                {opciones.name}
+              </option>
+            )
+          })
+        }
+      </select>
+
+
           </div>
-        )}
-      </div>
+
+
     </div>
   );
 };
