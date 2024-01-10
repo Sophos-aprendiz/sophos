ad667f03bd09b7618fafd2cbf9e015ea505cdb75
santiagor06 <santiago.restrepo06@hotmail.com>
2024-01-10 12:39:34 -0500

    cambios

--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -1,23 +1,29 @@
-import './Info.css'
-import {useState} from 'react'
-import ProjectSelector from './ProjectSelector';
+import "./Info.css";
+import { useState } from "react";
+import ProjectSelector from "./ProjectSelector";
 const Info = () => {
   const [dias, setDias] = useState({
-    lunes: '',
-    martes: '',
-    miercoles: '',
-    jueves:'',
-    viernes: '',
-    sabado: '',
-    domingo: '',
+    lunes: "",
+    martes: "",
+    miercoles: "",
+    jueves: "",
+    viernes: "",
+    sabado: "",
+    domingo: "",
   });
 
   const handleChange = (dia, value) => {
     // Verificar que el valor sea un número válido
-    const newValue = value == '' ? '' : parseFloat(value);
-    if (value === '' || (!isNaN(newValue) && newValue >= 0 && newValue <= 24 && newValue % 0.5 === 0)) {
+    const newValue = value == "" ? "" : parseFloat(value);
+    if (
+      value === "" ||
+      (!isNaN(newValue) &&
+        newValue >= 0 &&
+        newValue <= 24 &&
+        newValue % 0.5 === 0)
+    ) {
       // Actualizar el estado solo si el valor es válido
-      setDias(prevState => ({
+      setDias((prevState) => ({
         ...prevState,
         [dia]: newValue,
       }));
@@ -30,96 +36,92 @@ const Info = () => {
     return total;
   };
   return (
-    <div className='container-info'>
-      <div className='container-one'>
-        <select className='select'></select>
-        <select className='select'>
-        </select>
+    <div className="container-info">
+      <div className="container-one">
+        <select className="select"></select>
+
+        <select className="select"></select>
         <ProjectSelector />
-        <select className='select'>
-          
-        </select>
-        <select className='select'></select>
-        </div>
-      <div className='container-two'>
-         <input className='input-description' type="text"/>
+        <select className="select"></select>
       </div>
-      <div className='container-thre'>
+      <div className="container-two">
+        <input className="input-description" type="text" />
+      </div>
+      <div className="container-thre">
         <input
-          className='input-hour'
+          className="input-hour"
           type="number"
           step="0.5"
           min="0"
           max="24"
           value={dias.lunes}
-          onChange={(e) => handleChange('lunes', e.target.value)}
+          onChange={(e) => handleChange("lunes", e.target.value)}
         />
         <input
-          className='input-hour'
+          className="input-hour"
           type="number"
           step="0.5"
           min="0"
           max="24"
           value={dias.martes}
-          onChange={(e) => handleChange('martes', e.target.value)}
+          onChange={(e) => handleChange("martes", e.target.value)}
         />
         <input
-          className='input-hour'
+          className="input-hour"
           type="number"
           step="0.5"
           min="0"
           max="24"
           value={dias.miercoles}
-          onChange={(e) => handleChange('miercoles', e.target.value)}
+          onChange={(e) => handleChange("miercoles", e.target.value)}
         />
         <input
-          className='input-hour'
+          className="input-hour"
           type="number"
           step="0.5"
           min="0"
           max="24"
           value={dias.jueves}
-          onChange={(e) => handleChange('jueves', e.target.value)}
+          onChange={(e) => handleChange("jueves", e.target.value)}
         />
         <input
-          className='input-hour'
+          className="input-hour"
           type="number"
           step="0.5"
           min="0"
           max="24"
           value={dias.viernes}
-          onChange={(e) => handleChange('viernes', e.target.value)}
+          onChange={(e) => handleChange("viernes", e.target.value)}
         />
         <input
-          className='input-hour'
+          className="input-hour"
           type="number"
           step="0.5"
           min="0"
           max="24"
           value={dias.sabado}
-          onChange={(e) => handleChange('sabado', e.target.value)}
+          onChange={(e) => handleChange("sabado", e.target.value)}
         />
         <input
-          className='input-hour'
+          className="input-hour"
           type="number"
           step="0.5"
           min="0"
           max="24"
           value={dias.domingo}
-          onChange={(e) => handleChange('domingo', e.target.value)}
+          onChange={(e) => handleChange("domingo", e.target.value)}
         />
-        
-         <input
-          className='input-hour total'
+
+        <input
+          className="input-hour total"
           type="number"
           value={calcularTotal()}
           readOnly
-        /> 
+        />
       </div>
-      <button className='button-insert'>Insertar</button>
-
+      <button className="button-insert">Insertar</button>
     </div>
-  )
-}
+  );
+};
 
-export default Info
+export default Info;
