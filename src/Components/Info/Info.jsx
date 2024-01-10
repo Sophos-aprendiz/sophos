edf0945d38a28cb5e65cba16ba72a5ecf760e8f0
leruizr <leruizres1986@gmail.com>
2024-01-10 11:51:17 -0500

    se realizan pruebas y correciones en el insert

--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -1,138 +1,139 @@
-import './Info.css'
-import {useState} from 'react'
-import InsertTimeEntry from '../../hooks/InsertTimeEntry.jsx'
+// Importar las librerías necesarias
+import './Info.css';
+import { useState } from 'react';
+import useInsertTimeEntry from '../../hooks/InsertTimeEntry.jsx'; // Ajusta la ruta del import si es necesario
 
+// Componente Info
 const Info = () => {
+  // Estado para los días
   const [dias, setDias] = useState({
     lunes: '',
     martes: '',
     miercoles: '',
-    jueves:'',
+    jueves: '',
     viernes: '',
     sabado: '',
     domingo: '',
   });
 
+  // Función para manejar cambios en el input
   const handleChange = (dia, value) => {
-    // Verificar que el valor sea un número válido
-    const newValue = value == '' ? '' : parseFloat(value);
+    const newValue = value === '' ? '' : parseFloat(value);
     if (value === '' || (!isNaN(newValue) && newValue >= 0 && newValue <= 24 && newValue % 0.5 === 0)) {
-      // Actualizar el estado solo si el valor es válido
-      setDias(prevState => ({
+      setDias((prevState) => ({
         ...prevState,
         [dia]: newValue,
       }));
     }
   };
-  
 
+  // Función para calcular el total
   const calcularTotal = () => {
-    // Calcular la suma de los valores de los días
     const total = Object.values(dias).reduce((acc, curr) => acc + curr, 0);
     return total;
   };
-  
-  const insertTimeEntry = InsertTimeEntry(); // Obtén la función de useInsertTimeEntry
 
+  // Obtiene el objeto { insert, loading } del hook
+  const { postInsert, loading } = useInsertTimeEntry();
+
+  // Función para manejar la inserción
   const handleInsert = async () => {
     try {
-      // Aquí puedes utilizar la función insertTimeEntry para realizar la inserción
-      await insertTimeEntry(dias); // Asumiendo que insertTimeEntry acepta un objeto con días como argumento
-      console.log('Inserción exitosa'); // Opcional: Mostrar mensaje de éxito
+      await postInsert(dias);
+      console.log('Inserción exitosa');
     } catch (error) {
-      console.error('Error al insertar:', error); // Opcional: Mostrar mensaje de error
+      console.error('Error al insertar:', error);
     }
   };
-  
-  
+
+  // Renderizado del componente
   return (
     <div className='container-info'>
       <div className='container-one'>
         <select className='select'></select>
         <select className='select'></select>
         <select className='select'></select>
-        </div>
+      </div>
       <div className='container-two'>
-         <input className='input-description' type="text"/>
+        <input className='input-description' type='text' />
       </div>
       <div className='container-thre'>
         <input
           className='input-hour'
-          type="number"
-          step="0.5"
-          min="0"
-          max="24"
+          type='number'
+          step='0.5'
+          min='0'
+          max='24'
           value={dias.lunes}
           onChange={(e) => handleChange('lunes', e.target.value)}
         />
         <input
           className='input-hour'
-          type="number"
-          step="0.5"
-          min="0"
-          max="24"
+          type='number'
+          step='0.5'
+          min='0'
+          max='24'
           value={dias.martes}
           onChange={(e) => handleChange('martes', e.target.value)}
         />
         <input
           className='input-hour'
-          type="number"
-          step="0.5"
-          min="0"
-          max="24"
+          type='number'
+          step='0.5'
+          min='0'
+          max='24'
           value={dias.miercoles}
           onChange={(e) => handleChange('miercoles', e.target.value)}
         />
         <input
           className='input-hour'
-          type="number"
-          step="0.5"
-          min="0"
-          max="24"
+          type='number'
+          step='0.5'
+          min='0'
+          max='24'
           value={dias.jueves}
           onChange={(e) => handleChange('jueves', e.target.value)}
         />
         <input
           className='input-hour'
-          type="number"
-          step="0.5"
-          min="0"
-          max="24"
+          type='number'
+          step='0.5'
+          min='0'
+          max='24'
           value={dias.viernes}
           onChange={(e) => handleChange('viernes', e.target.value)}
         />
         <input
           className='input-hour'
-          type="number"
-          step="0.5"
-          min="0"
-          max="24"
+          type='number'
+          step='0.5'
+          min='0'
+          max='24'
           value={dias.sabado}
           onChange={(e) => handleChange('sabado', e.target.value)}
         />
         <input
           className='input-hour'
-          type="number"
-          step="0.5"
-          min="0"
-          max="24"
+          type='number'
+          step='0.5'
+          min='0'
+          max='24'
           value={dias.domingo}
           onChange={(e) => handleChange('domingo', e.target.value)}
         />
-        
-         <input
+        <input
           className='input-hour total'
-          type="number"
+          type='number'
           value={calcularTotal()}
           readOnly
-        /> 
+        />
       </div>
       <button className='button-insert' onClick={handleInsert}>
         Insertar
       </button>
-
     </div>
-  )
-}
+  );
+};
 
-export default Info
+// Exportar el componente Info
+export default Info;
--- src/hooks/InsertTimeEntry.jsx
+++ src/hooks/InsertTimeEntry.jsx
@@ -2,72 +2,75 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const InsertTimeEntry = () => {
-    const [token, setToken] = useState(''); 
-    const [insert, setInsert] = useState('');
-    const [loading, setLoading] = useState(true);
+const useInsertTimeEntry = () => {
+  const [token, setToken] = useState('');
+  const [insert, setInsert] = useState('');
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const getToken = async () => {
-            try {
-                const url = 'https://testapp.sophossolutions.com/SophosApiChronus/api/Token';
-                const credentials = {
-                    user: "UserSophosChronus.Api",
-                    password: "Sophos.2020*#"
-                };
-                const { data } = await axios.post(url, credentials);
-                setToken(data.token);
-            } catch (error) {
-                console.error("Error al obtener el token:", error);
-                setLoading(false); 
-            }
+  useEffect(() => {
+    const getToken = async () => {
+      try {
+        const url = 'https://testapp.sophossolutions.com/SophosApiChronus/api/Token';
+        const credentials = {
+          user: 'UserSophosChronus.Api',
+          password: 'Sophos.2020*#',
+        };
+        const { data } = await axios.post(url, credentials);
+        setToken(data.token);
+      } catch (error) {
+        console.error('Error al obtener el token:', error);
+        setLoading(false);
+      }
+    };
+
+    getToken();
+  }, []); // Llamada solo al montar el componente
+
+  const postInsert = async (dias) => {
+    try {
+      if (!token) {
+        console.error('Token no disponible. La inserción no se realizará.');
+        return;
+      }
+
+      const urlInsert =
+        'https://testapp.sophossolutions.com/SophosApiChronus/api/dbo/User/InsertTimeEntry';
+      const headers = {
+        'Content-Type': 'application/json',
+        accept: 'application/json',
+        Authorization: `Bearer ${token}`,
+      };
+
+      const body = {
+          categoryId: 805159,
+          timeEntryCreatorUserName: 'luis.ruizr',
+          timeEntryDescription: 'prueba postman',
+          timeEntryEstimateDuration: 0,
+          timeEntryEnteredDate: '2023-12-18',
+          timeEntryUserName: 'luis.ruizr',
+          timeEntryMonday: 0,
+          timeEntryTuesday: 0,
+          timeEntryWednesday: 0,
+          timeEntryThursday: 9,
+          timeEntryFriday: 9,
+          timeEntrySaturday: 0,
+          timeEntrySunday: 0,
+          isCompensatory: true,
+          timeEntryId: 0,
+          mensaje: 'string',
+          whodidit: 'luis.ruizr',
         };
 
-        const postInsert = async () => {
-            try {
-                const urlInsert = 'https://testapp.sophossolutions.com/SophosApiChronus/api/dbo/User/InsertTimeEntry';
-                const headers = {
-                    'Content-Type': 'application/json-patch+json',
-                    'accept': 'application/json',
-                    'Authorization': `Bearer ${token}` 
-                };
+      const response = await axios.post(urlInsert, body, { headers });
+      setInsert(response.data);
+    } catch (error) {
+      console.error('Error al insertar:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-                
-                const body = {
-                    "data": {
-                        "idTable": 0,
-                        "message": "string"
-                    },
-                    "meta": {
-                        "totalCount": 0,
-                        "pageSize": 0,
-                        "currentPage": 0,
-                        "totalPages": 0,
-                        "hasNextPage": true,
-                        "hasPreviusPage": true,
-                        "nextPageUrl": "string",
-                        "previusPageUrl": "string"
-                    }
-                };
-
-                const response = await axios.post(urlInsert, body, { headers });
-                setInsert(response.data);
-            } catch (error) {
-                console.error("Error al insertar:", error); 
-            } finally {
-                setLoading(false); 
-            }
-        };
-
-        getToken();
-        postInsert();
-    }, [token]); 
-
-    return (
-        <div>
-            {loading ? <p>Cargando...</p> : <p>Datos insertados: {JSON.stringify(insert)}</p>}
-        </div>
-    );
+  return { insert, loading, postInsert };
 };
 
-export default InsertTimeEntry;
+export default useInsertTimeEntry;
